c4e0a71b8b9f72436df26ddb9a7498b69508aa13
Marothi Mahlake <code_artist@outlook.com>
2022-08-18 08:17:09 +0200

    Work.js update

--- src/sections/work.js
+++ src/sections/work.js
@@ -1,6 +1,12 @@
+/** @jsx jsx */
+import { jsx } from 'theme-ui';
 import React, { useState } from 'react';
 import { Button } from 'theme-ui';
 import FsLightbox from 'fslightbox-react';
+
+import SectionHeader from '../components/section-header';
+
+import { IoMdEye } from 'react-icons/io';
 
 import Item1 from 'assets/work/01.jpg';
 import Item2 from 'assets/work/03.jpg';
@@ -15,33 +21,30 @@ function work() {
 
   return (
     <>
-      <div>
-        <Button onClick={() => setToggle(!toggle)} sx={styles.workSection}>See Our Work</Button>
+      <div
+        sx={{
+          pt: [7, 7, 7, 7, 7, 7, 7, 7],
+        }}
+      >
+        <SectionHeader slogan="CLICK BELOW TO SEE OUR WORK" title="Our Portfolio" />
+        <Button onClick={() => setToggle(!toggle)} sx={styles.workButton}>
+          <IoMdEye />
+          &nbsp;See Our Work
+        </Button>
       </div>
-      <FsLightbox
-        toggler={toggle}
-        sources={[
-          Item1,
-          Item2,
-          Item3,
-          Item4,
-          Item5,
-          Item6,
-          Item7
-        ]}
-      />
+      <FsLightbox toggler={toggle} sources={[Item1, Item2, Item3, Item4, Item5, Item6, Item7]} />
     </>
   );
 }
 
 const styles = {
-  workSection: {
+  workButton: {
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
     marginLeft: 'auto',
     marginRight: 'auto',
-    marginTop: '1.5rem'
+    textAlign: 'center',
   },
 };
 
